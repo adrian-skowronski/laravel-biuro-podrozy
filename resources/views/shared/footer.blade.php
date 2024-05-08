c2be4a5bda5b5644a84988e0e8e6b267cbd0ea47
Michal Szukala <michal.szukalapl@interia.pl>
2024-05-08 21:34:30 +0200

    second commit

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- resources/views/includes/footer.blade.php -->
+
+<footer class="container-fluid bg-body-tertiary"> 
+    <div class="row text-center pt-2"> 
+        <p>&copy; Biuro Podróży Skok &ndash; 2024</p> 
+    </div> 
+</footer> 
