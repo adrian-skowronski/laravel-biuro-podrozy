f9701edc4b32f0b0bec9f926009df5cf83f660a8
Michal Szukala <michal.szukalapl@interia.pl>
2024-05-11 04:01:48 +0200

    auth implemenation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HillController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +12,14 @@ Route::get('/oferty', function () {
     return view('oferty');
 })->name('oferty');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/hills', [HillController::class, 'index']);
+require __DIR__.'/auth.php';
